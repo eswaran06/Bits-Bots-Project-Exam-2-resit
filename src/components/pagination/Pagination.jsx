9e78df02c746f6a200c2b7648e6cb0413336794e
Eswaran Arulkumar <eswaran@oriondesign.no>
2022-11-15 11:55:23 +0100

    Responsive fix

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,38 +1,38 @@
-import React, { useEffect } from "react";
-import { useState } from "react";
-import ReactPaginate from "react-paginate";
-import { useDispatch, useSelector } from "react-redux";
-import { handlePagination } from "../../redux/features/pagination/paginationSlice";
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import ReactPaginate from 'react-paginate'
+import { useDispatch, useSelector } from 'react-redux'
+import { handlePagination } from '../../redux/features/pagination/paginationSlice'
 
 const Pagination = ({ products }) => {
-  const pageCount = useSelector((state) => state.pagination.pageCount);
-  const [event, setEvent] = useState({});
-  const dispatch = useDispatch();
+  const pageCount = useSelector((state) => state.pagination.pageCount)
+  const [event, setEvent] = useState({})
+  const dispatch = useDispatch()
   const handlePageClick = (event) => {
-    setEvent(event);
+    setEvent(event)
     // dispatch(handlePagination({ event, products }));
-  };
+  }
   useEffect(() => {
-    dispatch(handlePagination({ event, products }));
-  }, [dispatch, products, event]);
+    dispatch(handlePagination({ event, products }))
+  }, [dispatch, products, event])
   return (
     <div>
       <ReactPaginate
-        breakLabel="..."
-        nextLabel=" >"
+        breakLabel='...'
+        nextLabel=' >'
         onPageChange={handlePageClick}
-        pageRangeDisplayed={4}
+        pageRangeDisplayed={3}
         pageCount={pageCount}
-        previousLabel="< "
+        previousLabel='< '
         renderOnZeroPageCount={null}
-        containerClassName="pagination"
-        pageLinkClassName="page-num"
-        previousLinkClassName="page-num"
-        nextLinkClassName="page-num"
-        activeLinkClassName="active"
+        containerClassName='pagination'
+        pageLinkClassName='page-num'
+        previousLinkClassName='page-num'
+        nextLinkClassName='page-num'
+        activeLinkClassName='active'
       />
     </div>
-  );
-};
+  )
+}
 
-export default Pagination;
+export default Pagination
--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,49 +1,51 @@
-import React, { useEffect, useState } from "react";
-import Footer from "../../components/footer/Footer";
-import ProductCard from "../../components/productCard/ProductCard";
-import HeaderAndNavbar from "../../components/headerAndNavbar/HeaderAndNavbar";
-import Banner from "../../components/banner/Banner";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useEffect, useState } from 'react'
+import Footer from '../../components/footer/Footer'
+import ProductCard from '../../components/productCard/ProductCard'
+import HeaderAndNavbar from '../../components/headerAndNavbar/HeaderAndNavbar'
+import Banner from '../../components/banner/Banner'
+import { useDispatch, useSelector } from 'react-redux'
 
-import Spinner from "../../components/spinner/Spinner";
-import Pagination from "../../components/pagination/Pagination";
+import Spinner from '../../components/spinner/Spinner'
+import Pagination from '../../components/pagination/Pagination'
 
-import genreData from "../../assets/genreData";
-import isPropertyMatched from "../../assets/isMatched";
-import CategoryCard from "../../components/categoryCard/CategoryCard";
+import genreData from '../../assets/genreData'
+import isPropertyMatched from '../../assets/isMatched'
+import CategoryCard from '../../components/categoryCard/CategoryCard'
 
-import bannerImg from "../../assets/img/banner.jpg";
+import bannerImg from '../../assets/img/banner.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee, faSliders } from '@fortawesome/free-solid-svg-icons'
 
 const HomePage = () => {
-  const { isLoading, products, error } = useSelector((state) => state.products);
-  const currentItems = useSelector((state) => state.pagination.currentItems);
+  const { isLoading, products, error } = useSelector((state) => state.products)
+  const currentItems = useSelector((state) => state.pagination.currentItems)
 
-  const [categoryProducts, setCategoryProducts] = useState([]);
-
+  const [categoryProducts, setCategoryProducts] = useState([])
+  const [openCategory, setOpenCategory] = useState(false)
   useEffect(() => {
-    setCategoryProducts(currentItems);
-  }, [currentItems]);
+    setCategoryProducts(currentItems)
+  }, [currentItems])
 
   const handleFilterByGenre = (id) => {
-    const newArr = [];
+    const newArr = []
 
     for (let i = 0; i < products.length; i++) {
-      const checkPlatform = isPropertyMatched(products[i], id, "genres");
+      const checkPlatform = isPropertyMatched(products[i], id, 'genres')
       if (checkPlatform) {
-        newArr.push(products[i]);
+        newArr.push(products[i])
       }
     }
-    setCategoryProducts(newArr);
-  };
+    setCategoryProducts(newArr)
+  }
 
   return (
     <>
       {isLoading ? (
         <Spinner />
       ) : (
-        <div>
+        <div className='position-relative' id='top'>
           {/* Header And Navbar */}
-          <HeaderAndNavbar />
+          <HeaderAndNavbar setOpenCategory={setOpenCategory} />
 
           {/* Banner  */}
           <Banner bannerImg={bannerImg} />
@@ -59,37 +61,76 @@ const HomePage = () => {
               </div>
             </div>
           </section> */}
-          <section className="checkBox">
-            <div className="container">
-              <div className="checkBox_text">
-                <div className="d-md-none d-block filter">
-                  <a href="#">
-                    <i
-                      className="fa fa-sort-amount-desc"
-                      aria-hidden="true"
-                    ></i>{" "}
-                    filter
-                  </a>
-                </div>
-                <div className="row">
-                  {/* <!-- single item area start  --> */}
-                  <div className="col-md-3">
-                    <div className="left_checkBox_area_extra">
-                      <div className="left_checkBox_area">
-                        <div className="checkbox_area ">
+          <section className='checkBox '>
+            <div className='container'>
+              <div className='checkBox_text'>
+                <div className='d-flex gap-5  d-md-none d-block'>
+                  <div
+                    className='position-absolute '
+                    style={
+                      openCategory
+                        ? { left: '-5px', top: '65px', transition: '.5s' }
+                        : { left: '-1000px', transition: '.5s' }
+                    }
+                  >
+                    <div style={{ width: '300px' }}>
+                      <div
+                        style={{ backgroundColor: '#f2f2f1' }}
+                        className=' p-5'
+                      >
+                        <div className='checkbox_area '>
                           <h2>Genre</h2>
                           {genreData?.map((data) => (
-                            <label key={data.id} className="custom_checkbox_2">
+                            <label key={data.id} className='custom_checkbox_2'>
                               <input
                                 onChange={() => handleFilterByGenre(data.id)}
-                                type="checkbox"
-                                name="genre"
-                              />{" "}
+                                type='checkbox'
+                                name='genre'
+                              />
                               <p>
-                                {" "}
                                 <span>{data.name}</span>
                               </p>
-                              <span className="checkmark"></span>
+                              <span className='checkmark'></span>
+                            </label>
+                          ))}
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <a
+                    href='#top'
+                    onClick={() => setOpenCategory(!openCategory)}
+                    style={{
+                      zIndex: '100',
+                      fontSize: '30px',
+                      right: '8px',
+                      background: '#009444',
+                      color: 'white',
+                    }}
+                    className='position-fixed px-3 py-1 filter'
+                  >
+                    <FontAwesomeIcon icon={faSliders} />
+                  </a>
+                </div>
+                <div className='row'>
+                  {/* <!-- single item area start  --> */}
+                  <div className='col-md-3'>
+                    <div className='left_checkBox_area_extra'>
+                      <div className='left_checkBox_area'>
+                        <div className='checkbox_area '>
+                          <h2>Genre</h2>
+                          {genreData?.map((data) => (
+                            <label key={data.id} className='custom_checkbox_2'>
+                              <input
+                                onChange={() => handleFilterByGenre(data.id)}
+                                type='checkbox'
+                                name='genre'
+                              />{' '}
+                              <p>
+                                {' '}
+                                <span>{data.name}</span>
+                              </p>
+                              <span className='checkmark'></span>
                             </label>
                           ))}
                         </div>
@@ -97,17 +138,17 @@ const HomePage = () => {
                     </div>
                   </div>
                   {/* <!-- single item area start  --> */}
-                  <div className="col-md-9">
-                    <div className="right_checkBox_area">
-                      <div className="row g-5">
+                  <div className='col-md-9'>
+                    <div className='right_checkBox_area'>
+                      <div className='row g-5'>
                         {categoryProducts.length !== 0 ? (
                           categoryProducts.map((product) => (
                             <CategoryCard key={product.id} product={product} />
                           ))
                         ) : (
                           <p
-                            className="fw-bold"
-                            style={{ textAlign: "center", fontSize: "20px" }}
+                            className='fw-bold'
+                            style={{ textAlign: 'center', fontSize: '20px' }}
                           >
                             No games Available
                           </p>
@@ -128,7 +169,7 @@ const HomePage = () => {
         </div>
       )}
     </>
-  );
-};
+  )
+}
 
-export default HomePage;
+export default HomePage
